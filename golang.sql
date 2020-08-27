65ce7eb9b304756d485e6cae91567cd5b3ce55cf
gzstyp <444141300@qq.com>
2020-08-27 22:45:34 +0800

    在sql操作时把model传入时需要带&!!!???

---
+++ golang.sql
@@ -0,0 +1,43 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 192.168.3.66_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80021
+ Source Host           : 192.168.3.66:3306
+ Source Schema         : golang
+
+ Target Server Type    : MySQL
+ Target Server Version : 80021
+ File Encoding         : 65001
+
+ Date: 27/08/2020 22:44:45
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `updated_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `deleted_at` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `telephone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `telephone`(`telephone`) USING BTREE,
+  INDEX `idx_users_deleted_at`(`deleted_at`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, '2020-08-27 14:30:02', '2020-08-27 14:30:02', NULL, '田应平', '13765121695', '888888');
+INSERT INTO `users` VALUES (3, '2020-08-27 14:41:19', '2020-08-27 14:41:19', NULL, '田应平', '13765121690', '888888');
+
+SET FOREIGN_KEY_CHECKS = 1;
