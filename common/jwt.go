1ca1e3b7af3a2f3f6a9c3f1e3da445bbf66f2298
gzstyp <444141300@qq.com>
2020-08-28 11:07:41 +0800

    生成token,方法的定义!!! func CreateToken(user model.User) (string,error) {

---
+++ common/jwt.go
@@ -0,0 +1,38 @@
+package common
+
+import (
+	"com.fwtai/app2/model"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+//加密的密钥|盐值
+var jwtKey = []byte("a_secret_crect")
+
+//model|结构体|java的实体
+type Claims struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+//生成token,方法的定义!!!
+func CreateToken(user model.User) (string, error) {
+	//设置token的过期时间,7天 ==> time.Hour * 24 * 7
+	expirationTime := time.Now().Add(time.Hour * 24 * 7)
+	claims := &Claims{
+		UserId: user.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(), //过期时间
+			IssuedAt:  time.Now().Unix(),     //发放时间
+			Issuer:    "引路者|www.yinlz.com",   //发放人
+			Subject:   "用户token",             //主题
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey) //密钥加密
+	//如果生成错误,将错误返回
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
