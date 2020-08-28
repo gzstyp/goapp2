a6168178e18d335d9d98e78dbe4eba70e01bb14c
gzstyp <444141300@qq.com>
2020-08-28 08:56:14 +0800

    定义路由列表

--- route/routes.go
+++ route/routes.go
@@ -5,8 +5,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//在main.go调用
+//定义路由列表
 func CollectRoute(r *gin.Engine) *gin.Engine {
+	//注册功能
 	r.POST("/api/auth/register", controller.Register)
 	return r
 }
